f858bc26633e40f8ec0cb81931bb2534ea01652d
Dominik Zeiger <dz@ytrail.com>
2015-08-18 22:54:48 +0200

    attempt to fix error

--- run.sh
+++ run.sh
@@ -8,7 +8,9 @@ if [ ! -f /proc/net/if_inet6 ]; then
   sed -e '/listen \[::\]:80/ s/^#*/#/' -i /etc/nginx/sites-enabled/*
 fi
 
-sed -e "s/localhost:9200/${ES_HOST}:${ES_PORT}/" /opt/kibana/config/kibana.yml > /opt/kibana/config/kibana.yml
+cp /opt/kibana/config/kibana.yml /opt/kibana/config/old.yml
+cat /opt/kibana/config/kibana.yml
+sed -e "s/localhost:9200/${ES_HOST}:${ES_PORT}/" /opt/kibana/config/old.yml > /opt/kibana/config/kibana.yml
 
 cat << EOF > /usr/share/nginx/html/config.js
 define(['settings'],
