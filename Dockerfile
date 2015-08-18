1bb3c05d80b5ad717c5c3e4dda282e6050c8cbe2
Dominik Zeiger <dz@ytrail.com>
2015-08-18 23:05:00 +0200

    - moved service start to run script

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,6 @@ RUN tar xf /tmp/kibana.tar.gz -C /opt/kibana --strip-components=1
 ADD https://gist.githubusercontent.com/thisismitch/8b15ac909aed214ad04a/raw/bce61d85643c2dcdfbc2728c55a41dab444dca20/kibana4 /etc/init.d/kibana4
 RUN chmod +x /etc/init.d/kibana4
 RUN update-rc.d kibana4 defaults 96 9
-RUN service kibana4 start
 
 EXPOSE 80
 
--- run.sh
+++ run.sh
@@ -12,32 +12,6 @@ cp /opt/kibana/config/kibana.yml /opt/kibana/config/old.yml
 cat /opt/kibana/config/kibana.yml
 sed -e "s/localhost:9200/${ES_HOST}:${ES_PORT}/" /opt/kibana/config/old.yml > /opt/kibana/config/kibana.yml
 
-cat << EOF > /usr/share/nginx/html/config.js
-define(['settings'],
-function (Settings) {
-  return new Settings({
-    elasticsearch: "$ES_SCHEME://$ES_HOST:$ES_PORT",
-    default_route     : '/dashboard/file/logstash.json',
-    kibana_index: "kibana-int",
-    panel_names: [
-      'histogram',
-      'map',
-      'goal',
-      'table',
-      'filtering',
-      'timepicker',
-      'text',
-      'hits',
-      'column',
-      'trends',
-      'bettermap',
-      'query',
-      'terms',
-      'stats',
-      'sparklines'
-    ]
-  });
-});
-EOF
-
 nginx -c /etc/nginx/nginx.conf -g 'daemon off;'
+
+service kibana4 start
